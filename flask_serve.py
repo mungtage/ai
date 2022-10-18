0b1453ef45c89e609ba27bca7dd4b3efc9cad516
carryplz <qlshdh@naver.com>
2022-10-18 17:28:02 +0900

    [feat] 플라스크 기본 모델

--- flask_serve.py
+++ flask_serve.py
@@ -1,9 +1,100 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, jsonify, request
+import os
+import numpy as np
+from numpy.linalg import norm
+import PIL
+import time
+import math
+import dotenv
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.decomposition import PCA
+from flask_cors import CORS
+import urllib.request
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+dotenv.load_dotenv()
 
-app.run(host = '127.0.0.1', port=8080)
+# Port num
+PORT = os.getenv("PORT")
+
+
+@app.route('/pd', methods=["GET"])
+def predeict_image():
+    img_url = request.args.get("img")
+    if img_url != None:
+        path = '/content/drive/MyDrive/test/te'
+        file_list = os.listdir(path)
+
+        data_number = len(file_list)
+
+        if data_number == 0:
+            k_number = 0
+            exit()
+        else:
+            k_number = len(file_list)
+            components = 18
+
+        image_path = "/content/drive/MyDrive/test"
+        img_size = 256 #input size
+        model_resnet = ResNet50(weights='imagenet', include_top=False,input_shape=(img_size, img_size, 3),pooling='max')
+
+
+        batch_size = 64
+        img_generator = ImageDataGenerator(preprocessing_function = preprocess_input)
+        data_generator = img_generator.flow_from_directory(image_path, target_size = (img_size,img_size), batch_size = batch_size, class_mode = None, shuffle=False)
+
+        num_images = data_generator.samples
+        num_epochs = int(math.ceil(num_images / batch_size))
+
+        feature_list = model_resnet.predict(data_generator, num_epochs)
+
+        filenames = [image_path + '/' + s for s in data_generator.filenames]
+        pca = PCA(n_components = components)
+        pca.fit(feature_list)
+        compressed_features = pca.transform(feature_list)
+
+        neighbors = NearestNeighbors(n_neighbors=k_number, algorithm='ball_tree', metric='euclidean',radius = 1.6)
+        neighbors.fit(compressed_features)
+
+        search_image_url = img_url.split("?")[0]
+        urllib.request.urlretrieve(
+        search_image_url,
+        "d.jpg")
+
+        search_file = "d.jpg"
+        input_shape = (img_size, img_size, 3)
+        img = image.load_img(search_file, target_size=(input_shape[0], input_shape[1]))
+        img_array = image.img_to_array(img)
+        expanded_img_array = np.expand_dims(img_array, axis=0)
+        preprocessed_img = preprocess_input(expanded_img_array)
+        test_img_features = model_resnet.predict(preprocessed_img, batch_size=1)
+        image_test = pca.transform(test_img_features)
+        distance, indices = neighbors.kneighbors(image_test)
+
+
+        def similar_images(indices):
+            number = 1    
+            for index in indices:
+                if number <=len(indices) :
+                    print(filenames[index].split("/")[6])    
+                    number +=1
+
+        outputs = {"filename":similar_images(indices[0])}
+    return jsonify(outputs)
+
+if __name__ == "__main__":
+  img_size = 256 #input size
+  model_resnet = ResNet50(weights='imagenet', include_top=False,input_shape=(img_size, img_size, 3),pooling='max')
+  app.run(host="0.0.0.0", port=PORT, debug=True)
