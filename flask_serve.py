6cc3b762dbfa43ce949cf0d7349535b6873ca17b
carryplz <qlshdh@naver.com>
2022-11-02 21:17:22 +0900

    [feat] 등록 날짜 이후 이미지만 판단하는 기능 추가

--- flask_serve.py
+++ flask_serve.py
@@ -4,20 +4,20 @@ import os
 import requests
 import numpy as np
 from numpy.linalg import norm
-import boto3
 import PIL
 import time
 import math
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.efficientnet import EfficientNetB5, preprocess_input
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.decomposition import PCA
 from flask_cors import CORS
 import urllib.request
+import time
 
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
@@ -28,30 +28,42 @@ CORS(app)
 
 @app.route('/pd/', methods=["GET"])
 def predeict_image():
+    start = time.time()
     img_url = request.args.get("img")
-    if img_url != None:
-        path = '/home/mungtagepipe/pipeline/images/announcement'
+    date = request.args.get("happenDate")
+    if img_url != None and date != None:
+        path = '/home/aimungtage/images/announcement'
         file_list = os.listdir(path)
+        target_list = []
+        data_number = 0
+        for i in file_list:
+            if date <= i:
+                target_list.append(i)
 
-        data_number = len(file_list)
+        for i in target_list:
+            data_path = '/home/aimungtage/images/announcement/' + str(i)
+            data_number += len(os.listdir(data_path))
+
         if data_number == 0:
             k_number = 0
             components = 0
             exit()
-        elif data_number <= 4:
+        elif data_number <= 10:
             k_number = data_number
-            components = data_number
-        elif data_number <= 17:
-            k_number = 5
-            components = data_number
+            components = 4
+
+        elif data_number <= 50:
+            k_number = data_number
+            components = 10
         else:
-            k_number = data_number
+            k_number = 100
             components = 18
 
-        image_path = "/home/mungtagepipe/pipeline/images"
-        batch_size = 16
+        image_path = "/home/aimungtage/images/announcement"
+
+        batch_size = 8
         img_generator = ImageDataGenerator(preprocessing_function = preprocess_input)
-        data_generator = img_generator.flow_from_directory(image_path, target_size = (img_size,img_size), batch_size = batch_size, class_mode = None, shuffle=False)
+        data_generator = img_generator.flow_from_directory(image_path, target_size = (img_size,img_size), batch_size = batch_size, class_mode = None, shuffle=False, classes=target_list)
 
         num_images = data_generator.samples
         num_epochs = int(math.ceil(num_images / batch_size))
@@ -68,9 +80,9 @@ def predeict_image():
 
         search_image_url = img_url.split("?")[0]
         responds = requests.get(search_image_url)
-        search_image_name = search_image_url.split("/")[3]
-        print(search_image_name)
+        search_image_name = search_image_url.split("/")[-1]
         open(f"{search_image_name}", "wb").write(responds.content)
+        print(search_image_name)
 
         search_file = search_image_name
         input_shape = (img_size, img_size, 3)
@@ -87,13 +99,16 @@ def predeict_image():
             number = 1
             for index in indices:
                 if number <=len(indices):
-                    outputs[number-1]=os.path.splitext(os.path.basename(filenames[index]))[0]+".jpg"
+                    outputs[number-1]=os.path.splitext(os.path.basename(filenames[index]))[0]
                     number +=1
 
         similar_images(indices[0])
+        end = time.time()
+        print(f" 소요 시간 : {end - start}\n")
+
     return jsonify(outputs)
 
 if __name__ == "__main__":
   img_size = 256 #input size
-  model_resnet = ResNet50(weights='imagenet', include_top=False,input_shape=(img_size, img_size, 3),pooling='max')
+  model_resnet = EfficientNetB5(weights='imagenet', include_top=False,input_shape=(img_size, img_size, 3),pooling='max')
   app.run(host="0.0.0.0", port=5000, debug=True)
